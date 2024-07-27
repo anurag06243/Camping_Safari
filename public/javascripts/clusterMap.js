d156774efb32af8241aabe27c63e90fec6358dbc
anurag06243 <anuragkumar06243@gmail.com>
2024-07-27 11:30:27 +0530

    2nd commit

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -12,6 +12,8 @@ map.addControl(new mapboxgl.NavigationControl());
 map.on("load", function () {
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
+
+  // jdhgd;
   // add the point_count property to your source data.
   map.addSource("campgrounds", {
     type: "geojson",
